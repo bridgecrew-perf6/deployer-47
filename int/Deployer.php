510a49e272286ec9f86c809a237faeb5fb1aef02
spirling <johnie.spirling@gmail.com>
2020-12-10 19:23:36 +0200

    Initial commit

---
+++ int/Deployer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Spirling\Deployer\Contract;
+
+interface Deployer
+{
+
+}
---
+++ src/Deployer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Spirling\Deployer;
+
+use Spirling\Deployer\Contract\Deployer as DeployerInterface;
+
+class Deployer implements DeployerInterface
+{
+
+}
